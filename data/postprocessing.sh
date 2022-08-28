b6ae39161b7bf2282153edd0be713892dc724a5a
Sridhar Ratnakumar <srid@srid.ca>
2022-08-28 14:45:39 -0400

    agent

--- data/postprocessing.sh
+++ data/postprocessing.sh
@@ -3,5 +3,5 @@
 set -xeuo pipefail
 
 # TODO: This whole thing could be made a flake app.
-curl https://forums.redflagdeals.com/hot-deals-f9/trending/ > ./data/data.html
+curl -A "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"  https://forums.redflagdeals.com/hot-deals-f9/trending/ > ./data/data.html
 echo "<div>$(cat ./data/data.html | htmlq .topic_title_link)</div>" | xq | jq -r '.div.a' > ./data/data.json
