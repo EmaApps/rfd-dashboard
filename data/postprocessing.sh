05ffc851bd2f8742229a875c1edb5af926c0695f
Sridhar Ratnakumar <srid@srid.ca>
2022-08-28 14:32:50 -0400

    fuck apt

--- data/postprocessing.sh
+++ data/postprocessing.sh
@@ -2,6 +2,8 @@
 
 set -xeuo pipefail
 
-sudo apt-get install jq xq htmlq -y
+nix profile install nixpkgs#jq 
+nix profile install nixpkgs#yq
+nix profile install nixpkgs#htmlq
 
 echo "<div>`curl https://forums.redflagdeals.com/hot-deals-f9/trending/ | htmlq .topic_title_link`</div>" | xq | jq -r '.div.a' > ./data/data.json
--- data/postprocessing.ts
+++ data/postprocessing.ts
@@ -2,6 +2,7 @@ import 'https://deno.land/x/flat@0.0.14/mod.ts'
 
 // Forwards the execution to the bash script
 const bash_run = Deno.run({
+    // FIXME: when the script fails, we must fail.
     cmd: ['./data/postprocessing.sh'].concat(Deno.args),
 });
 
