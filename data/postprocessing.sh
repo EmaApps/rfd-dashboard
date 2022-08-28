e425320f1e272604c0a210c1a8c3cf4858f5ebea
Sridhar Ratnakumar <srid@srid.ca>
2022-08-28 14:42:56 -0400

    decouple

--- data/postprocessing.sh
+++ data/postprocessing.sh
@@ -3,4 +3,5 @@
 set -xeuo pipefail
 
 # TODO: This whole thing could be made a flake app.
-echo "<div>$(curl https://forums.redflagdeals.com/hot-deals-f9/trending/ | htmlq .topic_title_link)</div>" | xq | jq -r '.div.a' > ./data/data.json
+curl https://forums.redflagdeals.com/hot-deals-f9/trending/ > ./data/data.html
+echo "<div>$(cat ./data/data.html | htmlq .topic_title_link)</div>" | xq | jq -r '.div.a' > ./data/data.json
