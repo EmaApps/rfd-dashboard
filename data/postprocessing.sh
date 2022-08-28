b1a356362d23f780b0b09de4185aec6055bbae01
Sridhar Ratnakumar <srid@srid.ca>
2022-08-28 14:41:33 -0400

    well

--- data/postprocessing.sh
+++ data/postprocessing.sh
@@ -3,4 +3,4 @@
 set -xeuo pipefail
 
 # TODO: This whole thing could be made a flake app.
-echo "<div>`curl https://forums.redflagdeals.com/hot-deals-f9/trending/ | htmlq .topic_title_link`</div>" | xq | jq -r '.div.a' > ./data/data.json
+echo "<div>$(curl https://forums.redflagdeals.com/hot-deals-f9/trending/ | htmlq .topic_title_link)</div>" | xq | jq -r '.div.a' > ./data/data.json
