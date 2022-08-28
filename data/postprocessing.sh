4ed4cbe383af5390ded1637152af8f3c8005edef
Sridhar Ratnakumar <srid@srid.ca>
2022-08-28 14:37:06 -0400

    install in yaml

--- data/postprocessing.sh
+++ data/postprocessing.sh
@@ -2,8 +2,5 @@
 
 set -xeuo pipefail
 
-nix profile install nixpkgs#jq 
-nix profile install nixpkgs#yq
-nix profile install nixpkgs#htmlq
-
+# TODO: This whole thing could be made a flake app.
 echo "<div>`curl https://forums.redflagdeals.com/hot-deals-f9/trending/ | htmlq .topic_title_link`</div>" | xq | jq -r '.div.a' > ./data/data.json
