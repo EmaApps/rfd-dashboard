65e65183adcf2fd7d458ebbd658c372d4c4800a4
Sridhar Ratnakumar <srid@srid.ca>
2022-08-28 14:27:43 -0400

    try to jailbreak

--- data/postprocessing.sh
+++ data/postprocessing.sh
@@ -3,4 +3,6 @@ set -xeuo pipefail
 
 sudo apt-get install jq xq htmlq -y
 
+chmod u+w ./data/data.json
+
 echo "<div>`curl https://forums.redflagdeals.com/hot-deals-f9/trending/ | htmlq .topic_title_link`</div>" | xq | jq -r '.div.a' > ./data/data.json
